e666daad2e3b22f8765f01e0836703073a27012a
maks.user99 <maks.user99@gmail.com>
2022-01-10 16:51:54 +0200

    add textPath

--- index.js
+++ index.js
@@ -17,24 +17,30 @@ function createSvg() {
 
   let offset = -0.25;
   const totalSum = options.reduce((acc, current) => acc + current.weight, 0);
+
   const sections = [];
   const lines = [];
-  options.forEach(option => {
+  const textElements = [];
+
+  options.forEach((option, i) => {
     const optionWeight = option.weight / totalSum;
-    const { circle, line } = getSection(optionWeight, offset, option.text);
+    const { circle, line, text } = getSection(optionWeight, offset, option.text, i);
     sections.push(circle);
     lines.push(line);
+    textElements.push(text);
     offset += optionWeight;
   });
 
   svg.append(...sections);
   svg.append(...lines);
+  svg.append(...textElements);
   document.body.appendChild(svg);
 }
 
-function getSection(weight, offset, text) {
+function getSection(weight, offset, text, id) {
   const rotateDeg = 360 * offset;
-  
+  const textId = `line${id}`;
+
   const circle = document.createElementNS(svgns, 'circle');
   circle.setAttribute('r', 50);
   circle.setAttribute('cx', 100);
@@ -46,14 +52,25 @@ function getSection(weight, offset, text) {
   circle.setAttribute('transform-origin', 'center');
   circle.setAttribute('transform', `rotate(${rotateDeg})`);
 
-  const line = document.createElementNS(svgns, 'line');
+  const line = document.createElementNS(svgns, 'path');
+  line.setAttribute('id', textId);
+  line.setAttribute('fill', 'none');
   line.setAttribute('stroke', 'black');
-  line.setAttribute('x1', 100);
-  line.setAttribute('y1', 100);
-  line.setAttribute('x2', 100 + 100 * Math.cos((offset + weight / 2) * 2 * Math.PI));
-  line.setAttribute('y2', 100 + 100 * Math.sin((offset + weight / 2) * 2 * Math.PI));
+  const x0 = 100,
+    y0 = 100,
+    x1 = 100 + 100 * Math.cos((offset + weight / 2) * 2 * Math.PI),
+    y1 = 100 + 100 * Math.sin((offset + weight / 2) * 2 * Math.PI);
+  line.setAttribute('d', `M${x0} ${y0} L ${x1} ${y1}`);
 
-  return { circle, line };
+  const textPath = document.createElementNS(svgns, 'textPath');
+  textPath.setAttribute('href', `#${textId}`)
+  textPath.innerHTML = text;
+
+  const textEl = document.createElementNS(svgns, 'text');
+  // textEl.setAttribute('textLength', '50%')
+  textEl.appendChild(textPath);
+
+  return { circle, line, text: textEl };
 }
 
 function getRandomColor() {
